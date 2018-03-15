3c499cfb20b9c045d71f06aea960fca27db375c2
MercurieVV <mercurievv@gmail.com>
2018-03-16 00:29:16 +0200

    Update codegeneration

---
+++ src/main/scala/co/nextwireless/texts/ws/server/swagger/zapi/impl/package.scala
@@ -0,0 +1,15 @@
+package co.nextwireless.texts.ws.server.swagger.zapi
+
+import co.nextwireless.common.ws.http4s.{RequestProcessorExecuter}
+
+/**
+  * Created with IntelliJ IDEA.
+  * User: Victor Mercurievv
+  * Date: 3/16/2018
+  * Time: 12:06 AM
+  * Contacts: email: mercurievvss@gmail.com Skype: 'grobokopytoff' or 'mercurievv'
+  */
+package object impl {
+  type ExecutionHelper = RequestProcessorExecuter
+
+}
